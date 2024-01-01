51dc231285f80515ea3a82ea89e09483c3a16f3d
tarifalhasan <tarifalhasan999@gmail.com>
2024-01-01 17:33:33 +0600

    add:home hello

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -1,11 +1,64 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { OutlineButton, PrimaryButton } from "../../../components/button";
+import hp from "../../../lib/ResponsiveHeight";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView
+      style={{
+        paddingHorizontal: hp(2),
+      }}
+      className="flex-1 bg-white"
+    >
+      <View
+        style={{ paddingVertical: hp(5) }}
+        className="flex-row items-start justify-between"
+      >
+        <View>
+          <Text style={{ fontSize: hp(3) }} className="text-black font-normal">
+            Hello, <Text className="font-bold">Jenny</Text>
+          </Text>
+          <Text
+            style={{
+              fontSize: hp(2),
+            }}
+            className="tet-black opacity-[0.67] font-normal"
+          >
+            Welcome to Bakkro
+          </Text>
+        </View>
+        <View
+          style={{
+            width: hp(6),
+          }}
+        >
+          <Image
+            style={{
+              width: "100%",
+              height: hp(6),
+              objectFit: "cover",
+              borderRadius: 9,
+            }}
+            source={require("../../../assets/tarif.jpg")}
+          />
+          <View className="flex-row items-center gap-x-2">
+            <Text
+              style={{
+                fontSize: hp(1.7),
+              }}
+              className="text-black font-normal"
+            >
+              0.00
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View className="flex-row  space-x-4  justify-between">
+        <PrimaryButton btnStyle={"flex-1"} title={"Find accommodation"} />
+        <OutlineButton btnStyle={"flex-1"} title={"Purchase/Rental"} />
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ components/button.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import hp from "../lib/ResponsiveHeight";
+
+export const PrimaryButton = ({ title, btnStyle, ...props }) => {
+  return (
+    <TouchableOpacity
+      className={` ${btnStyle} bg-skin-primary flex-row justify-center items-center rounded-10 `}
+      style={{
+        height: hp(6),
+      }}
+      {...props}
+    >
+      <Text
+        style={{
+          fontSize: hp(1.8),
+        }}
+        className="text-white font-bold "
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+export const OutlineButton = ({ title, btnStyle, ...props }) => {
+  return (
+    <TouchableOpacity
+      className={` ${btnStyle} border rounded-md border-skin-primary flex-row justify-center items-center `}
+      style={{
+        height: hp(6),
+      }}
+      {...props}
+    >
+      <Text
+        style={{
+          fontSize: hp(1.8),
+          color: "#E97400",
+        }}
+        className=" font-bold "
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
